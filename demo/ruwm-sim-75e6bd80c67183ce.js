c7604b834455ecc3c0aef0e3583aaea13c5bf2f8
ivmarkov <ivan.markov@gmail.com>
2022-11-25 14:18:41 +0000

    Deploying to gh-pages from @ ivmarkov/ruwm@04db86f51ed4443706647f9a6fa6403e6cd0c2ff 🚀

--- demo/ruwm-sim-75e6bd80c67183ce.js
+++ demo/ruwm-sim-75e6bd80c67183ce.js
@@ -741,7 +741,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('ruwm-sim-ebb4a1d93469e721_bg.wasm', import.meta.url);
+        input = new URL('ruwm-sim-75e6bd80c67183ce_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
